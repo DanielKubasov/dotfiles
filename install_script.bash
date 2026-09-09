42eda611be0356f462d471319bf7537071acd3a6
Daniel <danil.kubasov2007@gmail.com>
2026-09-09 12:32:54 +0500

    feat: waybar config.

--- install_script.bash
+++ install_script.bash
@@ -1,22 +1,57 @@
 #!/usr/bin/env bash
 
+# TODO
+# 1. Install yay
+# 2. Install VSCode
+
 packages=(
+	# Applications
+	
 	firefox
 	telegram-desktop
-	rofi
-	waybar
-	git
-	neovim
+
+	# Utilities
+
+	blueman
+	btop
+	pavucontrol
+	brightnessctl
 	curl
 	wget
 	zip
 	unzip
+	debugedit
+	fakeroot
+	make
+	gcc
+
+	# UI
+	
+	rofi
+	waybar
+	hyprpaper
+	hyprlock
+
+	# Programming
+	
+	git
+	neovim
 	nvm
 	go
 	python-pip
 	jdk-openjdk
+
+	# Terminal
+	
 	ghostty
 	zsh
+	fzf
+
+	# Fonts
+	
+	ttf-nerd-fonts-symbols-common
+	otf-font-awesome
+	ttf-jetbrains-mono
 )
 
-sudo pacman -Sy "${packages[@]}"
+sudo pacman -Syu "${packages[@]}"
